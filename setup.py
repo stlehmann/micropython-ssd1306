60d42324e38da410adbaf59d04753b53069f1109
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2018-11-18 18:08:32 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+import sdist_upip
+
+setup(name='micropython-ssd1306',
+      version='0.1',
+      description='ssd1306 module for MicroPython',
+      long_description="",
+      url='https://github.com/stlehmann/micropython-ssd1306',
+      author='Stefan Lehmann',
+      author_email='stlm@posteo.de',
+      maintainer='Stefan Lehmann',
+      maintainer_email='stlm@posteo.de',
+      license='MIT',
+      cmdclass={'sdist': sdist_upip.sdist},
+py_modules=[''])
